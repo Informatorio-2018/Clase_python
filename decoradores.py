c89c4d2976c4b93fd2c3c4277ff90a744e630912
alexis_peloso <alexis.peloso@gmail.com>
2018-06-28 19:00:47 -0300

    decoradores valido

--- decoradores.py
+++ decoradores.py
@@ -1,28 +1,41 @@
 # Decoradores
 
+def mi_decorador(valido):
+	def _mi_decorador(funcion):
+		def funcionalida_1():
+			print("nueva funcionalidad 1")
+		def funcionalida_2():
+			print("nueva funcionalidad 2")
+		def nueva_funcion(*args):
+			if valido:
+				funcionalida_1()
+			else:
+				funcionalida_2()
+			funcion(*args)
+		return nueva_funcion
+	return _mi_decorador
 
-def mi_decorador(funcion):
-	def nueva_funcion():
-		print("nueva funcionalidad 1")
-		print("nueva funcionalidad 2")
-		funcion()
-		print("nueva funcionalidad 3")
-		print("nueva funcionalidad 4")
-	return nueva_funcion
-
-@mi_decorador
+@mi_decorador(valido = True)
 def saludo():
 	print("buen dia!")
-@mi_decorador
-def calcular():
-	print("el resultado es: ",10+6)
+
+
+@mi_decorador(valido = False)
+def calcular(a,b):
+	print("el resultado es: ",a+b)
+
+@mi_decorador(valido = True)
+def recalcular(a,b,c,d):
+	print("el resultado es: ",a+b+c+d)
 
 
 saludo()
+print()
+a=10
+b=8
+calcular(a,b)
+print()
 
-
-
-calcular()
-
+recalcular(a,b,4,3)
 
 
