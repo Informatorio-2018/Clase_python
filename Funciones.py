766c0b403ac0ab2126d538e884aae1bf8b5447d9
alexis_peloso <alexis.peloso@gmail.com>
2018-06-28 18:44:28 -0300

    funciones(args,**kwargs)

--- Funciones.py
+++ Funciones.py
@@ -12,3 +12,12 @@ print("el resulktado de la suma es:	",sumar(a,b))
 c = 2
 
 print("el resulktado de suma es:	",sumar(a,b,c))
+
+
+def nueva_funcion(**kwargs):
+	# print(kwargs)
+	print(kwargs.get("nombre","no existe"))
+	print(kwargs["edad"])
+
+
+nueva_funcion(nombre="juan", edad=44)
